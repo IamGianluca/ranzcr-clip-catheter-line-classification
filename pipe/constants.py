ab2023381cf5b35ab359ceebebde769f041a19df
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2021-01-18 13:18:20 -0500

    ML: use observed label frequencies in training set as predictions (#2)

---
+++ pipe/constants.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+path = Path('/home/gianluca/git/kaggle/ranzcr/')
+
+data_path = path / "data"
+train_img_path = data_path / "train"
+test_img_path = data_path / "test"
+
+metrics_path = path / "metrics"
