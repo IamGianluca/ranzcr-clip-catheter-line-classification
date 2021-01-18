70bf28ee4401f9ee452c3ff9b07ba6dbd5ae6037
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2021-01-18 16:56:33 -0500

    ML: add training step to DVC pipeline (#3)

--- pipe/train.py
+++ pipe/train.py
@@ -5,19 +5,22 @@ from sklearn.metrics import roc_auc_score
 from constants import data_path, metrics_path
 
 
-def train():
+def train(verbose: bool = False):
     df = pd.read_csv(data_path / 'train_folds.csv')
     label_cols = df.columns[1:-2]
 
     cv_scores = []
-    for fold in range(5):
+    for fold in range(df.kfold.max()):
         train, valid = df[df.kfold!=fold], df[df.kfold==fold]
+        if verbose:
+            print(f"Fold {fold}, {train.shape}, {valid.shape}")
+
+        y_pred = np.tile(train.loc[:, label_cols].mean(), (valid.shape[0], 1))
         y_true = valid.loc[:, label_cols].values
-        y_pred = np.tile(train.loc[:, label_cols].mean().values, (valid.shape[0], 1))
-        cv_scores.append(np.mean([roc_auc_score(y_true[:, i], y_pred[:, i]) for i in range(11)]))
+        cv_scores.append([roc_auc_score(y_true=y_true, y_score=y_pred, average="macro")])
 
     with open(metrics_path / 'model_one_cv.metric', 'w') as f:
-        f.write(f"AUC: {np.mean(cv_scores)}")
+        f.write(f"AUC: {np.mean(cv_scores):.3f}")
 
 
 if __name__ == '__main__':
