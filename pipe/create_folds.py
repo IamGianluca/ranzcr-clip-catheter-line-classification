647c8d982c4156f873809620c8b236aacf150cbf
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2021-01-24 21:20:41 -0500

    ML: use stratified k-fold cross-validation strategy (0.8218)

--- pipe/create_folds.py
+++ pipe/create_folds.py
@@ -1,6 +1,8 @@
 import constants
 import pandas as pd
-from sklearn.model_selection import KFold
+
+from pipe import constants
+from iterstrat.ml_stratifiers import MultilabelStratifiedKFold
 
 
 def split():
@@ -9,9 +11,15 @@ def split():
     df["kfold"] = -1
     df = df.sample(frac=1).reset_index(drop=True)
 
-    kf = KFold(n_splits=5)
-    for fold, (train_idx, val_idx) in enumerate(kf.split(X=df)):
+    x = df.drop(constants.target_cols, axis=1)
+    y = df.loc[:, constants.target_cols]
+    groups = df.PatientID
+
+    kf = MultilabelStratifiedKFold(n_splits=5, shuffle=True, random_state=16)
+    for fold, (train_idx, val_idx) in enumerate(kf.split(X=x, y=y)):
         df.loc[val_idx, "kfold"] = fold
+
+    print(df.groupby("kfold").mean())
 
     df.to_csv(constants.data_path / "train_folds.csv", index=False)
 
