15d42c7c73564a7a5e8512bfaa3147aaecdec49c
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2021-01-24 21:29:25 -0500

    ML: use stratified k-fold cross-validation strategy (0.8218) (#15)

--- pipe/constants.py
+++ pipe/constants.py
@@ -1,5 +1,20 @@
 from pathlib import Path
 
+
+target_cols = [
+    "ETT - Abnormal",
+    "ETT - Borderline",
+    "ETT - Normal",
+    "NGT - Abnormal",
+    "NGT - Borderline",
+    "NGT - Incompletely Imaged",
+    "NGT - Normal",
+    "CVC - Abnormal",
+    "CVC - Borderline",
+    "CVC - Normal",
+    "Swan Ganz Catheter Present",
+]
+
 path = Path("/home/gianluca/git/kaggle/ranzcr/")
 
 data_path = path / "data"
