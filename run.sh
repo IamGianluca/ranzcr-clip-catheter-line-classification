0b2eb02989a1e1263b5ec177c29999670564c508
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2021-01-31 18:48:21 -0500

    ML: tune ReduceLROnPlateau and EarlyStopping, 5-fold crossvalidation (0.8216) (#24)

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # resnet
 dvc repro train_resnet18_128 -fs
-dvc repro train_resnet34_128 -fs
-dvc repro train_resnet50_128 -fs
+# dvc repro train_resnet34_128 -fs
+# dvc repro train_resnet50_128 -fs
 
 # efficientnet
 
